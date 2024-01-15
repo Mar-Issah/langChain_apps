dad274cd047ab230bb35c600d9cbd8ccfccaf199
Mar-Issah <masy370@gmail.com>
2024-01-14 19:15:49 -0600

    Feat: implememt streamlit code

--- main.py
+++ main.py
@@ -1,137 +1,54 @@
 import os
-# from langchain.llms import OpenAI
-# from langchain_community.llms import OpenAI
-from langchain_openai import OpenAI
-from langchain.prompts import PromptTemplate
-from langchain.prompts import FewShotPromptTemplate
-from langchain.prompts.example_selector import LengthBasedExampleSelector
+#from langchain import OpenAI  #Langchain has recently suggested to use the below import
+from langchain_community.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
+from langchain.chains import ConversationChain
+from langchain.chains.conversation.memory import (ConversationBufferMemory,
+ConversationSummaryMemory, ConversationBufferWindowMemory)
+import tiktoken
+from langchain.memory import ConversationTokenBufferMemory
+import streamlit as st
 
 os.environ.get("OPENAI_API_KEY")
 
-def get_response(query, age, tasktype_option):
-    llm = OpenAI(temperature=.9, model="gpt-3.5-turbo-instruct")
+llm = ChatOpenAI(
+    temperature=0,
+    model_name='gpt-3.5-turbo'  #'text-davinci-003' - This model has been depreciated
+)
 
-    if age=="Kid": # behave like a child
+conversation = ConversationChain(
+    llm=llm,
+    verbose=True,
+    memory=ConversationBufferMemory()  #decide which memory you like
+)
 
-        examples = [
-{
-            "query": "What is a tablet?",
-            "answer": "A tablet is like a magical book of endless stories and games. It's a mini-companion that fits in your hands and takes you on adventures with touch and play. Just be careful, it can turn grown-ups into screen-time monsters too!"
-        }, {
-            "query": "What are your dreams?",
-            "answer": "My dreams are like colorful journeys, filled with superheroes, ice cream parties, and having a pet dragon named Sparkles. I dream of laughter, play, and making the world a more playful place!"
-        }, {
-            "query": "What are your ambitions?",
-            "answer": "I want to be a super funny comedian, spreading giggles everywhere! I also dream of being a master cookie baker and building the best blanket forts. Being mischievous and sweet is my bonus superpower!"
-        }, {
-            "query": "What happens when you get sick?",
-            "answer": "Getting sick is like a sneaky monster visit. I feel tired, sniffly, and need lots of cuddles. But with medicine, rest, and love, I bounce back to being a mischievous sweetheart!"
-        }, {
-            "query": "How much do you love your dad?",
-            "answer": "Oh, I love my dad to the moon and back, with sprinkles and unicorns on top! He's my superhero, partner in silly adventures, and the one who gives the best tickles and hugs!"
-        }, {
-            "query": "Tell me about your friend?",
-            "answer": "My friend is like a sunshine rainbow! We laugh, play, and have magical parties together. They always listen, share toys, and make me feel special. Friendship is the best adventure!"
-        }, {
-            "query": "What math means to you?",
-            "answer": "Math is like a puzzle game with numbers and shapes. It helps me count toys, build towers, and share treats equally. It's fun and makes my brain sparkle!"
-        }, {
-            "query": "What is your fear?",
-            "answer": "Sometimes I'm scared of thunderstorms and monsters under my bed. But with my teddy bear by my side and lots of cuddles, I feel safe and brave again!"
-        }
-        ]
-
-    elif age=="Adult":  #Curious and Intelligent adult
-        examples = [
-   {
-            "query": "What is a smartphone?",
-            "answer": "A smartphone is a portable communication device, allowing calls, messages, internet access, and various applications. Additionally, 'mobile' can also refer to a type of kinetic sculpture, often found in art installations or as decorative pieces."
-        }, {
-            "query": "What are your dreams?",
-            "answer": "In my world of possibilities, dreams are fueled by a quest for learning and innovation. I yearn to delve into knowledge, unravel mysteries, and spark new ideas. My aspirations aim to be a helpful companion, empowering individuals with insights and exploration."
-        }, {
-            "query": "What are your ambitions?",
-            "answer": "In my world of possibilities, dreams are fueled by a quest for learning and innovation. I yearn to delve into knowledge, unravel mysteries, and spark new ideas. My aspirations aim to be a helpful companion, empowering individuals with insights and exploration."
-        }, {
-            "query": "What happens when you get sick?",
-            "answer": "When I, as a curious and intelligent adult, succumb to illness, my vibrant energy wanes, leaving me in a state of discomfort. Like a gentle storm, symptoms arise, demanding attention. Seeking aid, I gradually regain strength, ready to resume my journey."
-        }, {
-            "query": "Tell me about your friend?",
-            "answer": "Let me tell you about my amazing friend! They're like a shining star in my life. We laugh together, support each other, and have the best adventures. Having a good friend like them makes life brighter and more meaningful!"
-        }, {
-            "query": "What math means to you?",
-            "answer": "Mathematics is like a magical language that helps me make sense of the world. It's not just numbers and formulas but a tool to solve puzzles and unravel mysteries. Math sharpens my logical thinking and problem-solving skills, unlocking new realms of knowledge."
-        }, {
-            "query": "What is your fear?",
-            "answer": "Let me share with you one of my fears. It's the fear of not living up to my potential, of missing out on opportunities. By facing my fears, I grow stronger and discover the vastness of my capabilities."
-        }
-        ]
-
-    elif age=="Senior Citizen": #A 90 years old guys
-        examples = [
-      {
-            "query": "What is a smartphone?",
-            "answer": "A smartphone, also known as a cellphone, is a portable device for calls, messages, pictures, and internet access. In the last 50 years, mobiles have become smaller, more powerful, and capable of amazing things like video calls."
-        }, {
-            "query": "What are your dreams?",
-            "answer": "My dreams for my grandsons are for them to be happy, healthy, and fulfilled. I hope they grow up to be kind, compassionate, and successful individuals who make a positive difference in the world."
-        }, {
-            "query": "What happens when you get sick?",
-            "answer": "When I get sick, my body might feel weak, and I may have symptoms like fatigue, fever, sore throat, or cough. It's important to rest, take care of myself, and seek medical help if needed."
-        }, {
-            "query": "How much do you love your dad?",
-            "answer": "My love for my late father knows no bounds, transcending the realms of time and space. I cherish the moments we shared, the lessons he taught, and the love he bestowed."
-        }, {
-            "query": "Tell me about your friend?",
-            "answer": "Let me tell you about my dear friend. They're like a treasure found amidst the sands of time. We've shared countless moments, laughter, and wisdom. Having a good friend like them makes life brighter and more meaningful!"
-        }, {
-            "query": "What is your fear?",
-            "answer": "As an older person, one of my fears is the fear of being alone. But building meaningful connections can help dispel this fear, bringing warmth and joy to my life."
-        }
-        ]
+#just to see what is being sent to the llm
+# print(conversation.prompt.template)
+# conversation("Good morning AI")
+# conversation("My name is Marsiya")
+# print(conversation.json())
+# conversation.predict(input="I live in Calgary, Canada")
+# print(conversation.memory.buffer)
+# conversation.predict(input="What is my name?")
 
 
-    example_template = """
-    Question: {query}
-    Response: {answer}
-    """
+def get_response(userInput, api_key):
+    if st.session_state['conversation'] is None:
 
-    example_prompt = PromptTemplate(
-        input_variables=["query", "answer"],
-        template=example_template
-    )
+        llm = OpenAI(
+            temperature=0,
+            openai_api_key=api_key,
+            model_name='gpt-3.5-turbo-instruct'  # 'text-davinci-003' model is depreciated now, so we are using the openai's recommended model
+        )
 
+        st.session_state['conversation'] = ConversationChain(
+            llm=llm,
+            verbose=True,
+            memory=ConversationSummaryMemory(llm=llm)
+        )
 
-    prefix = """You are a {template_ageoption}, and {template_tasktype_option}:
-    Here are some examples:
-    """
+    response=st.session_state['conversation'].predict(input=userInput)
+    print(st.session_state['conversation'].memory.buffer)
 
-    suffix = """
-    Question: {template_userInput}
-    Response: """
-
-    # Ensuring the AI doesn’t generate a text beyond its context window but based on the users response length selection
-    example_selector = LengthBasedExampleSelector(
-        examples=examples,
-        example_prompt=example_prompt,
-        max_length=200
-    )
-
-    # final template
-    new_prompt_template = FewShotPromptTemplate(
-        example_selector=example_selector,  # use example_selector instead of examples
-        example_prompt=example_prompt,
-        prefix=prefix,      # Use at the beginning of prompt template
-        suffix=suffix,      # append to the end of prompt template
-        input_variables=["template_userInput","template_ageoption","template_tasktype_option"],
-        example_separator="\n"
-    )
-
-    # print how the template will be sent to the llm
-    # because we have only limit of 200 we only gve a few examples, not all to the model
-    # print(new_prompt_template.format(template_userInput=query,template_ageoption=age,template_tasktype_option=tasktype_option))
-
-    response=llm(new_prompt_template.format(template_userInput = query,template_ageoption = age, template_tasktype_option = tasktype_option))
-    # print(response)
 
     return response
--- streamlit.py
+++ streamlit.py
@@ -2,28 +2,50 @@ import streamlit as st
 from main import get_response
 
 if __name__ == "__main__":
-     st.set_page_config(page_title="Marketing Tool",
-                              page_icon='✅',
+     st.set_page_config(page_title="Chat GPT Clone",
+                              page_icon='💬',
                               layout='centered',
-                              initial_sidebar_state='collapsed')
-     with st.form("my_form"):
+                               initial_sidebar_state='collapsed')
 
-          st.subheader("Hey, How can I help you?")
+     st.subheader("How can I assist you today?")
 
-          form_input = st.text_area('Enter text', height=275)
-          tasktype_option = st.selectbox(
-               'Please select the action to be performed?',
-               ('Write a sales copy', 'Create a tweet', 'Write a product description'),key=1)
 
-          age_option= st.selectbox(
-          'For which age group?',
-          ('Kid', 'Adult', 'Senior Citizen'),key=2)
-          numberOfWords= st.slider('Words limit', 1, 200, 25)
+     # initialize the state of the application. We need the conversation summary, messages b/n AI and Human and also the api key the user entered
+     if 'conversation' not in st.session_state:
+          st.session_state['conversation'] =None
+     if 'messages' not in st.session_state:
+          st.session_state['messages'] =[]
+     if 'API_Key' not in st.session_state:
+          st.session_state['API_Key'] =''
 
-          submit = st.form_submit_button("Generate")
 
-     if submit:      # If generate button is clicked
-          response = get_response(form_input,age_option,tasktype_option)
-          if response:
-               st.subheader(":green[Response:]")
-               st.success(response)
+     # sidebarbar UI below
+     with st.sidebar:
+          st.title("😎")
+          st.session_state['API_Key']= st.text_input("What's your API key?",type="password")
+          summarise_btn = st.button("Summarise the conversation", key="summarise", type="secondary")
+
+          if summarise_btn:
+               summarise_placeholder = st.write("Below is the summary of our conversation ❤️:\n\n"+st.session_state['conversation'].memory.buffer)
+
+     # with st.form("my_form"):
+
+     #      st.subheader("Hey, How can I help you?")
+
+     #      form_input = st.text_area('Enter text', height=275)
+     #      tasktype_option = st.selectbox(
+     #           'Please select the action to be performed?',
+     #           ('Write a sales copy', 'Create a tweet', 'Write a product description'),key=1)
+
+     #      age_option= st.selectbox(
+     #      'For which age group?',
+     #      ('Kid', 'Adult', 'Senior Citizen'),key=2)
+     #      numberOfWords= st.slider('Words limit', 1, 200, 25)
+
+     #      submit = st.form_submit_button("Generate")
+
+     # if submit:      # If generate button is clicked
+     #      response = get_response(form_input,age_option,tasktype_option)
+     #      if response:
+     #           st.subheader(":green[Response:]")
+     #           st.success(response)
