71d1abd865083dfa3ed7737970e71a05898d2f11
Mar-Issah <masy370@gmail.com>
2024-01-16 10:11:20 -0600

    Feat: add sample.txt file

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
 #from langchain import OpenAI  #Langchain has recently suggested to use the below import
 from langchain.llms import OpenAI
-from langchain.chains import ConversationChain
-from langchain.chains.conversation.memory import (ConversationBufferMemory,
-ConversationSummaryMemory, ConversationBufferWindowMemory)
-import tiktoken
-from langchain.memory import ConversationTokenBufferMemory
 import streamlit as st
 import os
 
 os.environ.get("OPENAI_API_KEY")
 
-def get_response(userInput):
-    if st.session_state['conversation'] is None:
-        llm = OpenAI(
-            temperature=0,
-            model_name='gpt-3.5-turbo-instruct'  # 'text-davinci-003' model is depreciated now
-        )
-        # same as creating the conversation var. but in the state
-        st.session_state['conversation'] = ConversationChain(
-            llm=llm,
-            verbose=True,
-            memory=ConversationSummaryMemory(llm=llm)
-        )
+# def get_response(userInput):
+#     if st.session_state['conversation'] is None:
+#         llm = OpenAI(
+#             temperature=0,
+#             model_name='gpt-3.5-turbo-instruct'  # 'text-davinci-003' model is depreciated now
+#         )
+#         # same as creating the conversation var. but in the state
+#         st.session_state['conversation'] = ConversationChain(
+#             llm=llm,
+#             verbose=True,
+#             memory=ConversationSummaryMemory(llm=llm)
+#         )
 
-    response = st.session_state['conversation'].predict(input=userInput)
-    # print(st.session_state['conversation'].memory.buffer) the summary
-    return response
+#     response = st.session_state['conversation'].predict(input=userInput)
+#     # print(st.session_state['conversation'].memory.buffer) the summary
+#     return response
