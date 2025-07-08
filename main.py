10b439a152abd15216e12387c118a374b4f9cd01
Marsiya Issah <masy370@gmail.com>
2025-07-07 20:59:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from langchain_google_genai import GoogleGenerativeAI
 
 # os.environ.get("HUGGINGFACEHUB_API_TOKEN")
 os.environ.get("OPENAI_API_KEY")
-os.environ.get("GOOGLE_API_KEY")
+#os.environ.get("GOOGLE_API_KEY")
 
 def answer_query(query):
-  #llm = OpenAI(model_name="gpt-4o-mini",temperature=0.5)
-  llm = GoogleGenerativeAI(model="gemini-2.0-flash")
+  llm = OpenAI(model_name="gpt-4o-mini",temperature=0.5)
+  #llm = GoogleGenerativeAI(model="gemini-2.0-flash")
 
   #llm = HuggingFaceHub(repo_id = "google/flan-t5-large")
   response = llm.invoke(query)
