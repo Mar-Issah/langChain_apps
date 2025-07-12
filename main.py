a27776a469b7156f9b0cbfcb844190418070b55c
Mar-Issah <masy370@gmail.com>
2025-07-12 17:44:52 -0400

    Update: upgrade langchain packages to newer versions

--- main.py
+++ main.py
@@ -1,45 +1,47 @@
 import os
-from langchain_community.llms import OpenAI
-#from langchain.llms import OpenAI
-#The above is no longer avialable, so replaced it with the below import
-from langchain_community.chat_models import ChatOpenAI
+from langchain_openai import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
-from langchain.tools import DuckDuckGoSearchRun
+from langchain_community.tools import DuckDuckGoSearchRun
+from dotenv import load_dotenv
 
+load_dotenv()
 
 os.environ.get("OPENAI_API_KEY")
 
-def get_script(prompt, video_length, temperature):
+
+def get_script(topic, video_length, temperature):
+    # Setting up OpenAI LLM
+    llm = OpenAI(temperature=temperature, model_name="gpt-4o-mini", max_tokens=1000)
+
+    #  DuckDuckGoSearchRun allows you to programmatically perform web searches
+    ddg_search = DuckDuckGoSearchRun()
+
     # Template for generating 'Title' based on the topic of the user
     title_template = PromptTemplate(
-        input_variables = ['subject'],
-        template='Please come up with a title for a YouTube video on {subject}.'
-        )
+        input_variables=["subject"],
+        template="Please come up with a title for a YouTube video on {subject}.",
+    )
 
     # Template for generating 'Video Script' using DuckDuckGo search engine
-    # Please install DuckDuckGo
     script_template = PromptTemplate(
-        input_variables = ['title', 'DuckDuckGo_Search','duration'],
-        template='Create a script for a YouTube video based on this title. TITLE: {title} of duration: {duration} minutes using this search data {DuckDuckGo_Search} '
+        input_variables=["title", "DuckDuckGo_Search", "duration"],
+        template="Create a script for a YouTube video based on this title. TITLE: {title} of duration: {duration} minutes using this search data {DuckDuckGo_Search} ",
     )
-        #Setting up OpenAI LLM
-    llm = ChatOpenAI(temperature=temperature, model_name='gpt-3.5-turbo')
 
-    #Creating chains for 'Title' & 'Video Script'
-    title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True)
-    script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True)
-
-    # https://python.langchain.com/docs/modules/agents/tools/integrations/ddg
-    search = DuckDuckGoSearchRun()
+    # Creating chains for 'Title' & 'Video Script'
+    title_chain = LLMChain(llm=llm, prompt=title_template, verbose=False)
+    script_chain = LLMChain(llm=llm, prompt=script_template, verbose=False)
 
     # Executing the chains we created for 'Title'
-    title = title_chain.run(prompt)
+    title = title_chain.run(topic)
 
     # Executing the chains we created for 'Video Script' by taking help of search engine 'DuckDuckGo'
-    search_result = search.run(prompt)
-    script = script_chain.run(title= title, DuckDuckGo_Search= search_result, duration=video_length)
+    search_result = ddg_search.run(topic)
 
-    # Returning the output
+    script = script_chain.run(
+        title=title, DuckDuckGo_Search=search_result, duration=video_length
+    )
+
+    # Returning the outputs
     return search_result, title, script
-
--- streamlit.py
+++ streamlit.py
@@ -3,39 +3,44 @@ from main import get_script
 
 
 if __name__ == "__main__":
-     st.set_page_config(page_title="Video script writer",
-                              page_icon='🎞️',
-                              layout='centered')
+    st.set_page_config(
+        page_title="Video script writer", page_icon="🎞️", layout="centered"
+    )
 
-     st.markdown("<h3 style='text-align: center;'>Video Script Writing Tool</h3>", unsafe_allow_html=True)
+    st.markdown(
+        "<h3 style='text-align: center;'>Video Script Writing Tool</h3>",
+        unsafe_allow_html=True,
+    )
 
+    # # Creating Session State Variable
+    # if 'API_Key' not in st.session_state:
+    #      st.session_state['API_Key'] =''
 
-     # # Creating Session State Variable
-     # if 'API_Key' not in st.session_state:
-     #      st.session_state['API_Key'] =''
-
-     prompt = st.text_input('Enter the topic of the video',key="prompt")
-     video_length = st.text_input('What is the expected length of the video (in minutes)', key="video_length")
-     temperature = st.slider('How creative should the script be - (0 LOW || 1 HIGH)', 0.0, 1.0, 0.2,step=0.1) # temperature
+    topic = st.text_input("Enter the topic of the video", key="topic")
+    video_length = st.text_input(
+        "What is the expected length of the video (in minutes)", key="video_length"
+    )
+    temperature = st.slider(
+        "How creative should the script be - (0 LOW || 1 HIGH)", 0.0, 1.0, 0.2, step=0.1
+    )  # temperature
 
 submit = st.button("Generate Script")
 
 if submit:
-     with st.container():
-        search_result,title,script = get_script(prompt,video_length, temperature)
-        #Let's generate the script
-        st.subheader('Please find below')
+    with st.container():
+        search_result, title, script = get_script(topic, video_length, temperature)
+        # Let's generate the script
+        st.subheader("Please find below")
 
-        #Display Title
+        # Display Title
         st.subheader(":green[Title:]")
         st.success(title)
 
-        #Display Video Script
+        # Display Video Script
         st.subheader(":green[Your Video Script:📝]")
         st.success(script)
 
-        #Display Search Engine Result
+        # Display Search Engine Result
         st.subheader("Check Out the DuckDuckGo Search:")
-        with st.expander('Click to open'):
+        with st.expander("Click to open"):
             st.info(search_result)
-
