9e9c0dea9628c6baa7b9c6ac938a05e5f4fc41e2
Mar-Issah <masy370@gmail.com>
2025-07-15 19:44:43 -0400

    update: integrate langsmith api to monitor llm repsonse

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-#from langchain_huggingface import HuggingFaceHub
+# from langchain_huggingface import HuggingFaceHub
 import os
 from langchain_openai import OpenAI
 from langchain_google_genai import GoogleGenerativeAI
+from dotenv import load_dotenv
 
-# os.environ.get("HUGGINGFACEHUB_API_TOKEN")
+load_dotenv()
+
+os.environ.get("LANGCHAIN_API_KEY")
 os.environ.get("OPENAI_API_KEY")
-#os.environ.get("GOOGLE_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = (
+    "true"  # THis mean that the entire app will be trace when we execute the code
+)
+
 
 def answer_query(query):
-  llm = OpenAI(model_name="gpt-4o-mini",temperature=0.5)
-  #llm = GoogleGenerativeAI(model="gemini-2.0-flash")
+    llm = OpenAI(model_name="gpt-4o-mini", temperature=0.5)
+    # llm = GoogleGenerativeAI(model="gemini-2.0-flash")
 
-  #llm = HuggingFaceHub(repo_id = "google/flan-t5-large")
-  response = llm.invoke(query)
-  return response
+    # llm = HuggingFaceHub(repo_id = "google/flan-t5-large")
+    response = llm.invoke(query)
+    return response
